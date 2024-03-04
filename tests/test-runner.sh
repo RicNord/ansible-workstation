a23e84fc64504a000c20a0706d86459c7cf7fd45
Richard <git.nordstrom@gmail.com>
2024-03-04 20:47:36 +0100

    terrafrom to test-runner

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#terraform apply
-incus exec arch-vm -- bash -c "pacman -S --noconfirm git base-devel reflector"
-incus exec arch-vm -- bash -c "reflector --delay 3 -c 'se,dk,no,fi,ge,nl' -f 20 --sort=rate -p https --save '/etc/pacman.d/mirrorlist'"
-incus exec arch-vm -- bash -c "cd /tmp \
+cd ./terraform/ && terraform apply -auto-approve
+incus exec --project ansible-ws arch-vm -- bash -c "pacman -S --noconfirm git base-devel reflector"
+incus exec --project ansible-ws arch-vm -- bash -c "reflector --delay 3 -c 'se,dk,no,fi,ge,nl' -f 20 --sort=rate -p https --save '/etc/pacman.d/mirrorlist'"
+incus exec --project ansible-ws arch-vm -- bash -c "cd /tmp \
     && git clone --shallow-submodules --recurse-submodules --depth 1 https://github.com/ricnord/ansible-workstation \
     ; cd ansible-workstation \
     && make install"
