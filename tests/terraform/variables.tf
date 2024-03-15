a453079be9addb4236bc0d1066d53c6bf4b34503
Richard <git.nordstrom@gmail.com>
2024-03-15 23:13:03 +0100

    add conditional instance for terraform

---
+++ tests/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "arch-container" {
+  description = "Arch container enabled"
+  default     = true
+}
+
+variable "arch-vm" {
+  description = "Arch VM enabled"
+  default     = true
+}
+
+variable "ubuntu-container" {
+  description = "Ubuntu container enabled"
+  default     = true
+}
+
+variable "ubuntu-vm" {
+  description = "Ubuntu VM enabled"
+  default     = true
+}
