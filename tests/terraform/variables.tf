82c5a91165a9bb6163345997327fb950c9316b0d
Richard <git.nordstrom@gmail.com>
2024-03-16 22:17:01 +0100

    add selection of instances in test-runner

--- tests/terraform/variables.tf
+++ tests/terraform/variables.tf
@@ -1,19 +1,9 @@
-variable "arch-container" {
-  description = "Arch container enabled"
-  default     = true
-}
-
-variable "arch-vm" {
-  description = "Arch VM enabled"
-  default     = true
-}
-
-variable "ubuntu-container" {
-  description = "Ubuntu container enabled"
-  default     = true
-}
-
-variable "ubuntu-vm" {
-  description = "Ubuntu VM enabled"
-  default     = true
+variable "instance-list" {
+  default     = []
+  type        = list(any)
+  description = "List of instances names to deploy"
+  validation {
+    condition     = alltrue([for instance in var.instance-list : contains(["arch-vm", "arch-container", "ubuntu-vm", "ubuntu-container"], instance)]) || length(var.instance-list) == 0
+    error_message = "Valid instance namse are arch-vm, arch-container, ubuntu-vm, ubuntu-container"
+  }
 }
