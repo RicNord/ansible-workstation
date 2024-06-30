1d548ae9c7806571c91e3556a86048c7fdddb63d
Richard Nordström <git.nordstrom@gmail.com>
2024-06-30 22:48:41 +0200

    add local files exec option to test runner

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 
 INSTANCE_LIST=''
 UBUNTU_VERSIONS='22.04,24.04'
+LOCAL_FILES=false
 _CURRENT_DIR="$(dirname "$0")"
 
 # Colors
@@ -13,20 +14,22 @@ NC='\033[0m' # NO COLOR
 
 function usage() {
     cat <<EOF
-    Usage: $0 [ -i instance1,instance2... ]
+    Usage: $0 [ -i instance1,instance2... ] [ -u version1,version2... ] [ -l ]
 
     -i    comma separeated list of instances
     -u    comma separeated list of ubuntu versions (default: 22.04,24.04)
+    -l    use local version of files instead of current branch in git remote
 
 EOF
     exit 1
 }
 
 # Parse args
-while getopts "i:u:" opt; do
+while getopts "i:u:l" opt; do
     case "${opt}" in
         i) INSTANCE_LIST=$OPTARG ;;
         u) UBUNTU_VERSIONS=$OPTARG ;;
+        l) LOCAL_FILES=true ;;
         *) usage ;;
     esac
 done
@@ -60,15 +63,22 @@ run_ansible() {
     incus exec "$1" --project ansible-ws -- bash -c "\
         rm -rf /tmp/ansible-workstation
         "
-    incus exec "$1" --project ansible-ws -- bash -c "\
-        git clone \
-        --single-branch \
-        --branch $(git branch --show-current) \
-        --shallow-submodules \
-        --recurse-submodules \
-        --depth 1 \
-        https://github.com/ricnord/ansible-workstation \
-        /tmp/ansible-workstation "
+    if [ "$LOCAL_FILES" == true ]; then
+        incus file push "$(git rev-parse --show-toplevel)" "$1/tmp/" \
+            --project ansible-ws \
+            --recursive \
+            --create-dirs
+    else
+        incus exec "$1" --project ansible-ws -- bash -c "\
+            git clone \
+            --single-branch \
+            --branch $(git branch --show-current) \
+            --shallow-submodules \
+            --recurse-submodules \
+            --depth 1 \
+            https://github.com/ricnord/ansible-workstation \
+            /tmp/ansible-workstation "
+    fi
     incus exec --project ansible-ws "$1" -- bash -c "\
         chmod u+x /tmp/ansible-workstation/run-ansible.sh"
     incus exec --project ansible-ws "$1" -- bash -c "\
@@ -83,34 +93,20 @@ terraform_apply
 
 get_project_instances
 
+export LOCAL_FILES
 export -f run_ansible
 
-if [ -n "${INSTANCE_LIST}" ]; then
-    echo -e "${BBlue}Testing selected instances... \n${NC}"
-    echo "$ALL_INSTANCES"
-    printf "\n"
-    echo -e "${BBlue}Running... Writing output to: ${NC}\n\t /tmp/.ansible-output/ \n"
-    parallel \
-        --tagstring '{}' \
-        --results /tmp/.ansible-output/{}/ \
-        --joblog /tmp/.ansible-ws-test.log \
-        run_ansible ::: "$ALL_INSTANCES" || true
+echo -e "${BBlue}Testing instances... \n${NC}"
+echo "$ALL_INSTANCES"
+printf "\n"
+echo "Start time: $(date +%T)"
+echo -e "${BBlue}Running... Writing output to: ${NC}\n\t /tmp/.ansible-output/ \n"
+parallel \
+    --tagstring '{}' \
+    --results /tmp/.ansible-output/{}/ \
+    --joblog /tmp/.ansible-ws-test.log \
+    run_ansible ::: "$ALL_INSTANCES" || true
 
-    printf "\n"
-    echo -e "${BBlue}Test result log: ${NC}"
-    column -t /tmp/.ansible-ws-test.log
-else
-    echo -e "${BBlue}Testing all instances... \n${NC}"
-    echo "$ALL_INSTANCES"
-    printf "\n"
-    echo -e "${BBlue}Running... Writing output to: ${NC}\n\t /tmp/.ansible-output/ \n"
-    parallel \
-        --tagstring '{}' \
-        --results /tmp/.ansible-output/{}/ \
-        --joblog /tmp/.ansible-ws-test.log \
-        run_ansible ::: "$ALL_INSTANCES" || true
-
-    printf "\n"
-    echo -e "${BBlue}Test result log: \n${NC}"
-    column -t /tmp/.ansible-ws-test.log
-fi
+printf "\n"
+echo -e "${BBlue}Test result log: ${NC}"
+column -t /tmp/.ansible-ws-test.log
